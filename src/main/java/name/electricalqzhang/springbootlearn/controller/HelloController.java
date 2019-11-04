60d7fd5fb5aab7e4eb14a48850f6061b422f18b8
Pioneer4 <electricalqzhang@gmail.com>
2019-11-04 10:16:57 +0800

    IEDA结合Spring boot开发技巧

--- src/main/java/name/electricalqzhang/springbootlearn/controller/HelloController.java
+++ src/main/java/name/electricalqzhang/springbootlearn/controller/HelloController.java
@@ -1,12 +1,20 @@
 package name.electricalqzhang.springbootlearn.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import name.electricalqzhang.springbootlearn.model.Article;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
     @RequestMapping("/hello")
-    public String hello(String name) {
-        return "Hello world, " + name;
+    public String hello() {
+        Article article = new Article();
+        article.setAuthor("zhao");
+
+        log.info("Slf4j的测试内容：" + article.toString());
+
+        return "Hello world, " + article;
     }
 }
